fb954737f0b55ae2a6b231cf6b1dfbfb84efb8dc
Евгений <fgvngfdf@gmail.com>
2021-03-30 19:54:30 +0300

    Fix DictionaryListTests

--- Tests/DictionaryListTests.cs
+++ Tests/DictionaryListTests.cs
@@ -9,102 +9,91 @@ namespace Tests
 	[TestFixture]
 	class DictionaryListTests
 	{
-		Dictionary<Person, string> personDictionary = new Dictionary<Person, string>();
-		Dictionary<Person, string> constantPersonDictionary = new Dictionary<Person, string>();
-		List<Person> personList = new List<Person>();
-		List<Person> constantPersonList = new List<Person>();
-
 		Random rn = new Random();
-		delegate void GenerateDictionaty(ref Dictionary<Person, string> keyValuePairs, IGetHashCode getHashCode, int val);
-		delegate void GenerateList(ref List<Person> people, IGetHashCode getHashCode, int val);
+		delegate void GenerateDictionaty(IGetHashCode getHashCode, int val);
+		delegate void GenerateList(IGetHashCode getHashCode, int val);
 
 		[Test]
 		public void RunDictionaryPerson()
 		{
-			GenerateDictionaty generate = CreatePerson;
-			CheckTimeGenerateDictionary(100, generate, ref personDictionary, new ImplementationBaseGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateDictionary(10000, generate, ref personDictionary, new ImplementationBaseGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateDictionary(20000, generate, ref personDictionary, new ImplementationBaseGetHashCode());
+			GenerateDictionaty generate = CreatePersonDictionary;
+			CheckTimeGenerateDictionary(100, generate, new ImplementationBaseGetHashCode());
+			CheckTimeGenerateDictionary(10000, generate, new ImplementationBaseGetHashCode());
+			CheckTimeGenerateDictionary(20000, generate, new ImplementationBaseGetHashCode());
 		}
 
 		[Test]
-		public void RunDictionaryConstantPerson()
+		public void RunDictionaryPersonConstant()
 		{
-			GenerateDictionaty generate = CreatePerson;
-			CheckTimeGenerateDictionary(100, generate, ref constantPersonDictionary, new ImplementationConstGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateDictionary(10000, generate, ref constantPersonDictionary, new ImplementationConstGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateDictionary(20000, generate, ref constantPersonDictionary, new ImplementationConstGetHashCode());
+			GenerateDictionaty generate = CreatePersonDictionary;
+			CheckTimeGenerateDictionary(100, generate, new ImplementationConstGetHashCode());
+			CheckTimeGenerateDictionary(10000, generate, new ImplementationConstGetHashCode());
+			CheckTimeGenerateDictionary(20000, generate, new ImplementationConstGetHashCode());
 		}
 
 		[Test]
 		public void RunListPerson()
 		{
-			GenerateList generate = CreatePerson;
-			CheckTimeGenerateList(100, generate, ref personList, new ImplementationBaseGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(10000, generate, ref personList, new ImplementationBaseGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(20000, generate, ref personList, new ImplementationBaseGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(50000, generate, ref personList, new ImplementationBaseGetHashCode());
+			GenerateList generate = CreatePersonList;
+			CheckTimeGenerateList(100, generate, new ImplementationBaseGetHashCode());
+			CheckTimeGenerateList(10000, generate, new ImplementationBaseGetHashCode());
+			CheckTimeGenerateList(20000, generate, new ImplementationBaseGetHashCode());
+			CheckTimeGenerateList(50000, generate, new ImplementationBaseGetHashCode());
 		}
 
 		[Test]
-		public void RunConstantListPerson()
+		public void RunListPersonConstant()
 		{
-			GenerateList generate = CreatePerson;
-			CheckTimeGenerateList(100, generate, ref constantPersonList, new ImplementationConstGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(10000, generate, ref constantPersonList, new ImplementationConstGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(20000, generate, ref constantPersonList, new ImplementationConstGetHashCode());
-			personDictionary.Clear();
-			CheckTimeGenerateList(50000, generate, ref constantPersonList, new ImplementationConstGetHashCode());
+			GenerateList generate = CreatePersonList;
+			CheckTimeGenerateList(100, generate, new ImplementationConstGetHashCode());
+			CheckTimeGenerateList(10000, generate, new ImplementationConstGetHashCode());
+			CheckTimeGenerateList(20000, generate, new ImplementationConstGetHashCode());
+			CheckTimeGenerateList(50000, generate, new ImplementationConstGetHashCode());
 		}
 
-		private void CheckTimeGenerateList(int val, GenerateList func, ref List<Person> keyValuePairs, IGetHashCode getHashCode)
+		private void CheckTimeGenerateList(int count, GenerateList func, IGetHashCode getHashCode)
 		{
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
-			func(ref keyValuePairs, getHashCode, val);
+			func(getHashCode, count);
 			stopwatch.Stop();
-			Console.WriteLine($"Время генерации для {val} объектов: {stopwatch.ElapsedTicks}");
+			Console.WriteLine($"Время генерации для {count} объектов: {stopwatch.ElapsedMilliseconds}");
 		}
 
-		private void CheckTimeGenerateDictionary(int val, GenerateDictionaty func, ref Dictionary<Person, string> keyValuePairs , IGetHashCode getHashCode)
+		private void CheckTimeGenerateDictionary(int count, GenerateDictionaty func, IGetHashCode getHashCode)
 		{
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
-			func(ref keyValuePairs, getHashCode, val);
+			func(getHashCode, count);
 			stopwatch.Stop();
-			Console.WriteLine($"Время генерации для {val} объектов: {stopwatch.ElapsedTicks}");
+			Console.WriteLine($"Время генерации для {count} объектов: {stopwatch.ElapsedMilliseconds}");
 		}
 
-		private void CreatePerson(ref Dictionary<Person, string> keyValuePairs, IGetHashCode getHashCode, int val)
+		private void CreatePersonDictionary(IGetHashCode getHashCode, int count)
 		{
-			for (int i = 0; i < val; i++)
+			Dictionary<Person, string> persons = new Dictionary<Person, string>();
+
+			for (int i = 0; i < count; i++)
 			{
-				int RandomValue = rn.Next(1, 29);
-				Person person = new Person(new NameSurname(Convert.ToString((Enums.Names)RandomValue), Enums.CorrectSurname(RandomValue, rn.Next(1, 33)), Enums.CorrectPatronymic(RandomValue, rn.Next(1, 19))),
+				int randomValue = rn.Next(1, 29);
+				Person person = new Person(new NameSurname(Convert.ToString((Enums.Names)randomValue), Enums.CorrectSurname(randomValue, rn.Next(1, 33)), Enums.CorrectPatronymic(randomValue, rn.Next(1, 19))),
 					new DateTime(rn.Next(1960, 2001), rn.Next(1, 12), rn.Next(1, 28)), Convert.ToString((Enums.City)rn.Next(1, 7)), rn.Next(1000000, 10000000), getHashCode);
 
-				keyValuePairs.Add(person, Enums.Workplace[rn.Next(0, 10)]);
+				persons.Add(person, Enums.Workplace[rn.Next(0, 10)]);
 			}
 		}
 
-		private void CreatePerson(ref List<Person> keyValuePairs, IGetHashCode getHashCode, int val)
+		private void CreatePersonList(IGetHashCode getHashCode, int count)
 		{
-			for (int i = 0; i < val; i++)
+			List<Person> persons = new List<Person>();
+
+			for (int i = 0; i < count; i++)
 			{
-				int RandomValue = rn.Next(1, 29);
-				Person person = new Person(new NameSurname(Convert.ToString((Enums.Names)RandomValue), Enums.CorrectSurname(RandomValue, rn.Next(1, 33)), Enums.CorrectPatronymic(RandomValue, rn.Next(1, 19))),
+				int randomValue = rn.Next(1, 29);
+				Person person = new Person(new NameSurname(Convert.ToString((Enums.Names)randomValue), Enums.CorrectSurname(randomValue, rn.Next(1, 33)), Enums.CorrectPatronymic(randomValue, rn.Next(1, 19))),
 					new DateTime(rn.Next(1960, 2001), rn.Next(1, 12), rn.Next(1, 28)), Convert.ToString((Enums.City)rn.Next(1, 7)), rn.Next(1000000, 10000000), getHashCode);
 
-				keyValuePairs.Add(person);
+				persons.Add(person);
 			}
 		}
 	}
